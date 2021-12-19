f5a6c9fc21244272d7e29dd5358eed58cf1fc4dc
Oleg Efimov <0raziel0@gmail.com>
2021-12-19 23:57:25 +0300

    Initial commit

---
+++ Swift Spotify Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Spotify Clone
+//
+//  Created by Олег Ефимов on 19.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
